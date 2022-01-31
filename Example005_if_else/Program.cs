6ff8eef1589ea7bfdd3f12bf5421bc42887cedfe
vMarhil <marxill@mail.ru>
2022-01-31 18:35:59 +0500

    Example005_if_else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.Readline();
+
+if(username.ToLower() == "маша");
+{
+    Console.Write("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет,  ");
+    Console.WriteLine(username);
+}
